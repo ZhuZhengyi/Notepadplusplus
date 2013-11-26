a2c121637d30990a3c985fa4254beea5f65bd11a
Lioncash <mathew1800@gmail.com>
2013-11-26 10:34:39 -0500

    Bugfix: Add missing break in switch statement in BabyGrid.cpp.

--- PowerEditor/src/WinControls/Grid/BabyGrid.cpp
+++ PowerEditor/src/WinControls/Grid/BabyGrid.cpp
@@ -1975,6 +1975,7 @@ LRESULT CALLBACK GridProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			{
 				BGHS[SelfIndex].textcolor = RGB(0, 0, 0);
 			}
+			break;
 
 		case WM_MOUSEMOVE:
 			int x = LOWORD(lParam);
